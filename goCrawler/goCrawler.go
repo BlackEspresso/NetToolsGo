1af861f54afd5e5c014e40fc6b049c5e8c11e0dd
Espresso <sweet.black.espresso@googlemail.com>
2016-03-01 09:01:25 +0100

    add http file store

--- goCrawler/goCrawler.go
+++ goCrawler/goCrawler.go
@@ -1,4 +1,3 @@
-// gocrawler
 package main
 
 import (
@@ -6,53 +5,42 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+type JsonHttp struct {
+	URL        string
+	Body       string
+	Header     http.Header
+	Status     string
+	StatusCode int
+}
+
 type HttpTransaction struct {
-	Response     http.Response
-	DoneTime     int64
-	Duration     float64
-	RequestBody  string
-	ResponseBody string
-	Tags         []string
+	Request  JsonHttp
+	Response JsonHttp
+	DoneTime int64
+	Duration float64
 }
 
-func NewFromResponse(res *http.Response, reqBody io.Reader, donetime int64, duration float64) HttpTransaction {
-	ht := HttpTransaction{}
-	ht.Response = *res
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err == nil {
-		ht.ResponseBody = string(body)
-	}
-
-	if reqBody != nil {
-		body, err = ioutil.ReadAll(reqBody)
-		if err == nil {
-			ht.RequestBody = string(body)
-		}
-	}
-
-	ht.DoneTime = donetime
-	ht.Duration = duration
-
-	return ht
-}
+var fileStorageUrl string = ""
 
 func main() {
 	urlFlag := flag.String("url", "", "url, e.g. http://www.google.com")
+	fileStorage := flag.String("filestore", "http://localhost:8079/file/7363a35f-f411-4751-96ec-2d19b5a22323", "url to filestore")
 	delayFlag := flag.Int("delay", 1000, "delay, in milliseconds, default is 1000ms=1sec")
 	flag.Parse()
+
+	fileStorageUrl = *fileStorage
 
 	if *urlFlag == "" {
 		log.Fatal("no url provided.")
@@ -64,6 +52,58 @@ func main() {
 	links := make(map[string]bool)
 	links[*urlFlag] = false // startsite
 	fetchSites(links, *delayFlag)
+}
+
+func fetchSites(links map[string]bool, delayMs int) {
+	for {
+		urlStr, found := getNextSite(links)
+		if !found {
+			return // done
+		}
+
+		links[urlStr] = true
+		fmt.Println("parsing site: " + urlStr)
+
+		ht, err := DoGETTransaction(urlStr)
+		content, err := json.Marshal(ht)
+		checkerror(err)
+
+		fileName := strconv.FormatInt(ht.DoneTime, 10) + ".httpt"
+		saveCrawl(urlStr, fileName, content)
+
+		// fetch
+		buf := bytes.NewBufferString(ht.Response.Body)
+		doc, err := goquery.NewDocumentFromReader(buf)
+		checkerror(err)
+		// parse content
+		findLinks(urlStr, doc, links)
+		time.Sleep(time.Duration(delayMs) * time.Millisecond)
+	}
+}
+
+func TransactionFromResponse(res *http.Response, url string, duration float64) *HttpTransaction {
+	ht := HttpTransaction{}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err == nil {
+		ht.Response.Body = string(body)
+	}
+	ht.Request.Header = res.Request.Header
+	ht.Request.URL = url
+
+	if res.Request.Body != nil {
+		body, err = ioutil.ReadAll(res.Request.Body)
+		if err == nil {
+			ht.Request.Body = string(body)
+		}
+	}
+
+	ht.Response.Status = res.Status
+	ht.Response.StatusCode = res.StatusCode
+	ht.DoneTime = time.Now().Unix()
+	ht.Duration = duration
+
+	return &ht
 }
 
 func DoGETTransaction(url string) (*HttpTransaction, error) {
@@ -82,57 +122,8 @@ func DoGETTransaction(url string) (*HttpTransaction, error) {
 	}
 
 	durSec := time.Now().Sub(starttime).Seconds()
-	nowSec := time.Now().Unix()
-	ht := NewFromResponse(resp, nil, nowSec, durSec)
-	return &ht, nil
-}
-
-func fetchSites(links map[string]bool, delayMs int) {
-	for {
-		urlStr, found := getNextSite(links)
-		if !found {
-			return // done
-		}
-
-		links[urlStr] = true
-		fmt.Println("parsing site: " + urlStr)
-
-		ht, err := DoGETTransaction(urlStr)
-		if err != nil {
-			fmt.Printf("skipping: " + err.Error())
-			continue
-		}
-		if ok, _ := PathExists("./sites/"); !ok {
-			os.Mkdir("./sites", 0777)
-		}
-
-		f, err := os.Create("./sites/" + strconv.FormatInt(ht.DoneTime, 10) + ".httpt")
-		checkerror(err)
-		defer f.Close()
-
-		content, err := json.Marshal(ht)
-		checkerror(err)
-		f.Write(content)
-
-		// fetch
-		buf := bytes.NewBufferString(ht.ResponseBody)
-		doc, err := goquery.NewDocumentFromReader(buf)
-		checkerror(err)
-		// parse content
-		findLinks(urlStr, doc, links)
-		time.Sleep(time.Duration(delayMs) * time.Millisecond)
-	}
-}
-
-func PathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
+	ht := TransactionFromResponse(resp, url, durSec)
+	return ht, nil
 }
 
 func getNextSite(links map[string]bool) (string, bool) {
@@ -176,4 +167,61 @@ func checkerror(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func saveCrawl(crawledUri string, fileName string, content []byte) {
+	params := map[string]string{"meta": crawledUri}
+
+	req, err := newfileUploadRequest(fileStorageUrl, params, "upload", fileName, content)
+	if err != nil {
+		log.Fatal("cant create file store request ", err)
+	}
+
+	c := http.Client{}
+	c.Timeout = time.Duration(200) * time.Second
+
+	uploadSuccess := false
+	for retries := 0; retries < 3; retries++ {
+		cresp, err := c.Do(req)
+		if err != nil {
+			log.Println("file store", err)
+			continue
+		}
+		if cresp.StatusCode != 200 {
+			log.Println("file store response ", cresp.StatusCode)
+			continue
+		}
+		uploadSuccess = true
+		break
+	}
+
+	if !uploadSuccess {
+		log.Println("error while saving")
+		log.Println(fileName, len(content), fileStorageUrl)
+		log.Fatal("exiting")
+	}
+}
+
+func newfileUploadRequest(uri string, params map[string]string, paramName string, fName string,
+	fileContent []byte) (*http.Request, error) {
+
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, fName)
+	if err != nil {
+		return nil, err
+	}
+	part.Write(fileContent)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, _ := http.NewRequest("POST", uri, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, err
 }
